54fdc62b12bb46a4f938d66259ee24856e2a6155
tkman904 <cjfdn0904@gmail.com>
2025-10-01 17:54:03 +0900

    웹_8일차 (51일차)

---
+++ CSSServletProject/src/main/java/com/sist/main/FoodDetail.java
@@ -0,0 +1,18 @@
+package com.sist.main;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/FoodDetail")
+public class FoodDetail extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
